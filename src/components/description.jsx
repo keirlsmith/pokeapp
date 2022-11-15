c375a1e36b8f4242d4d19c285012011ea4a017b5
keirlsmith <97139230+keirlsmith@users.noreply.github.com>
2022-11-14 20:34:36 -0500

    Start Description 1

--- src/components/description.jsx
+++ src/components/description.jsx
@@ -28,7 +28,36 @@ const PokeThumbnail = ({
                 <small>#0{id}</small>
             </div>
             <img src={image} alt={name} />
-            <div className="detail-wrapper"></div>
+            <div className="detail-wrapper">
+                <h3>{name.toUpperCase()}</h3>
+                <small>Type : {type}</small>
+                <button className="pokeinfo"
+                onClick={() => setShow(!show)}>
+                    {show === true ? "Know less..." : "Know more..."}
+                </button>
+                {show === true ? (
+                    <description
+                        weightpok = {weight}
+                        heightpok = {height}
+                        pokstat1={stat1}
+                        pokstat2={stat2}
+                        pokstat3={stat3}
+                        pokstat4={stat4}
+                        pokstat5={stat5}
+                        pokstat6={stat6}
+                        posbs1={bs1}
+                        posbs2={bs2}
+                        posbs3={bs3}
+                        posbs4={bs4}
+                        posbs5={bs5}
+                        posbs6={bs6}
+                    />
+                ) : (
+                    <></>        
+                )}
+            </div>
         </div>
-    )
-}
+    );
+};
+
+export default PokeThumbnail;
