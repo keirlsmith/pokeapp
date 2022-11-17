35c75b499b7e1136ad0e9f89a5e0bab7cc995437
keirlsmith <97139230+keirlsmith@users.noreply.github.com>
2022-11-16 20:40:33 -0500

    Adding CSS

--- src/components/description.jsx
+++ src/components/description.jsx
@@ -1,63 +1,63 @@
-import React, {useState} from "react";
-import description from "./description";
+import React from "react";
 
-const PokeThumbnail = ({
-    id,
-    name,
-    type,
-    height,
-    weight,
-    stat1,
-    stat2,
-    stat3,
-    stat4,
-    stat5,
-    stat6,
-    bs1,
-    bs2,
-    bs3,
-    bs4,
-    bs5,
-    bs6,
+const description = ({
+    heightpok,
+    weightpok,
+    pokstat1,
+    pokstat2,
+    pokstat3,
+    pokstat4,
+    pokstat5,
+    pokstat6,
+    posbs1,
+    posbs2,
+    posbs3,
+    posbs4,
+    posbs5,
+    posbs6,
 }) => {
-    const style = `thumb-container ${type}`;
-    const [show, setShow] = useState(false);
     return (
-        <div className={style}>
-            <div className="number">
-                <small>#0{id}</small>
-            </div>
-            <img src={image} alt={name} />
-            <div className="detail-wrapper">
-                <h3>{name.toUpperCase()}</h3>
-                <small>Type : {type}</small>
-                <button className="pokeinfo"
-                onClick={() => setShow(!show)}>
-                    {show === true ? "Know less..." : "Know more..."}
-                </button>
-                {show === true ? (
-                    <description
-                        weightpok = {weight}
-                        heightpok = {height}
-                        pokstat1={stat1}
-                        pokstat2={stat2}
-                        pokstat3={stat3}
-                        pokstat4={stat4}
-                        pokstat5={stat5}
-                        pokstat6={stat6}
-                        posbs1={bs1}
-                        posbs2={bs2}
-                        posbs3={bs3}
-                        posbs4={bs4}
-                        posbs5={bs5}
-                        posbs6={bs6}
-                    />
-                ) : (
-                    <></>        
-                )}
-            </div>
+        <div className="desc">
+            <p>
+                <b>Height</b> is <b>{heightpok * 10} cm.</b>
+            </p>
+            <p>
+                <b>Weight</b> is <b>{weightpok * 0.1} kg</b>
+            </p>
+
+            <h3>Stat</h3>
+            <p>
+                <b>
+                    {pokstat1} : {posbs1}
+                </b>
+            </p>
+            <p>
+                <b>
+                    {pokstat2} : {posbs2}
+                </b>
+            </p>
+            <p>
+                <b>
+                    {pokstat3} : {posbs3}
+                </b>
+            </p>
+            <p>
+                <b>
+                    {pokstat4} : {posbs4}
+                </b>
+            </p>
+            <p>
+                <b>
+                    {pokstat5} : {posbs5}
+                </b>
+            </p>
+            <p>
+                <b>
+                    {pokstat6} : {posbs6}
+                </b>
+            </p>
         </div>
     );
 };
 
-export default PokeThumbnail;
+export default description;
